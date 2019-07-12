c8b340140f237fb76a2e1cd9957b2dbff4f167bf
zebnat <zebnat@gmail.com>
2019-07-12 21:34:38 +0200

    fix: fixed adapters code that had unnecesary state
    adapters held state which is not what you expect from an adapter

--- src/lib/ProdjsonToVideoAdapter.ts
+++ src/lib/ProdjsonToVideoAdapter.ts
@@ -3,87 +3,43 @@ import { Video, ProdVideoJSON, Thumb } from '../common/types'
 export default class ProdjsonToVideoAdapter implements Video {
   private _prodJSON: ProdVideoJSON
 
-  private _visible: boolean = true
-  private _title: string = ''
-  private _rating: number = 0
-  private _daysAgo: number = 0
-  private _subs: number = 0
-  private _thumb: Thumb = { height: 0, url: '', width: 0 }
-  private _href: string = ''
-  private _categories: string[] = []
-
   constructor(prodJSON: ProdVideoJSON) {
     this._prodJSON = prodJSON
   }
 
   get visible(): boolean {
-    this._visible = this._prodJSON.v
-    return this._visible
+    return this._prodJSON.v
   }
+
   set visible(value: boolean) {
-    this._visible = value
+    this._prodJSON.v = value
   }
 
   get title(): string {
-    this._title = this._prodJSON.t
-    return this._title
-  }
-
-  set title(value: string) {
-    this._title = value
+    return this._prodJSON.t
   }
 
   get rating(): number {
-    this._rating = this._prodJSON.r
-    return this._rating
-  }
-
-  set rating(value: number) {
-    this._rating = value
+    return this._prodJSON.r
   }
 
   get daysAgo(): number {
-    this._daysAgo = this._prodJSON.d
-    return this._daysAgo
-  }
-
-  set daysAgo(value: number) {
-    this._daysAgo = value
+    return this._prodJSON.d
   }
 
   get thumb(): Thumb {
-    this._thumb = this._prodJSON.tm
-    return this._thumb
-  }
-
-  set thumb(value: Thumb) {
-    this._thumb = value
+    return this._prodJSON.tm
   }
 
   get href(): string {
-    this._href = this._prodJSON.h
-    return this._href
-  }
-
-  set href(value: string) {
-    this._href = value
+    return this._prodJSON.h
   }
 
   get categories(): string[] {
-    this._categories = this._prodJSON.c
-    return this._categories
-  }
-
-  set categories(value: string[]) {
-    this._categories = value
+    return this._prodJSON.c
   }
 
   get subs(): number {
-    this._subs = this._prodJSON.s
-    return this._subs
-  }
-
-  set subs(value: number) {
-    this._subs = value
+    return this._prodJSON.s
   }
 }
--- src/lib/devjsonToVideoAdapter.ts
+++ src/lib/devjsonToVideoAdapter.ts
@@ -3,192 +3,86 @@ import { Video, DevVideoJSON, Thumb } from '../common/types'
 export default class DevjsonToVideoAdapter implements Video {
   private _devJSON: DevVideoJSON
 
-  private _visible: boolean = false
-  private _title: string = ''
-  private _rating: number = 0
-  private _daysAgo: number = 0
-  private _thumb: Thumb = { height: 0, width: 0, url: '' }
-  private _href: string = ''
-  private _categories: string[] = []
-  private _subs: number = 0
-
-  private _videoId: string | undefined = undefined
-  private _trendCategoryPosition: number | undefined = undefined
-  private _status: any = undefined
-  private _stats: any = undefined
-  private _spam: number | undefined = undefined
-  private _normalize: number[] = []
-  private _lang: string | undefined = undefined
-  private _details: any = undefined
-  private _channelId: string | undefined = undefined
-  private _categoryId: string | undefined = undefined
-
   constructor(devJSON: DevVideoJSON) {
     this._devJSON = devJSON
   }
 
   // no optional
   get visible(): boolean {
-    this._visible = this._devJSON.visible
-    return this._visible
+    return this._devJSON.visible
   }
 
   set visible(value: boolean) {
-    this._visible = value
+    this._devJSON.visible = value
   }
 
   get title(): string {
-    this._title = this._devJSON.title
-    return this._title
-  }
-
-  set title(value: string) {
-    this._title = value
+    return this._devJSON.title
   }
 
   get rating(): number {
-    this._rating = this._devJSON.rating
-    return this._rating
-  }
-
-  set rating(value: number) {
-    this._rating = value
+    return this._devJSON.rating
   }
 
   get daysAgo(): number {
-    this._daysAgo = this._devJSON.daysAgo
-    return this._daysAgo
-  }
-
-  set daysAgo(value: number) {
-    this._daysAgo = value
+    return this._devJSON.daysAgo
   }
 
   get thumb(): Thumb {
-    this._thumb = this._devJSON.thumb
-    return this._thumb
-  }
-
-  set thumb(value: Thumb) {
-    this._thumb = value
+    return this._devJSON.thumb
   }
 
   get href(): string {
-    this._href = this._devJSON.href
-    return this._href
-  }
-
-  set href(value: string) {
-    this._href = value
+    return this._devJSON.href
   }
 
   get categories(): string[] {
-    this._categories = this._devJSON.categories
-    return this._categories
-  }
-
-  set categories(value: string[]) {
-    this._categories = value
+    return this._devJSON.categories
   }
 
   get subs(): number {
-    this._subs = this._devJSON.subs
-    return this._subs
-  }
-
-  set subs(value: number) {
-    this._subs = value
+    return this._devJSON.subs
   }
 
   // optional
 
   get videoId(): string | undefined {
-    this._videoId = this._devJSON.videoId
-    return this._videoId
-  }
-
-  set videoId(value: string | undefined) {
-    this._videoId = value
+    return this._devJSON.videoId
   }
 
   get trendCategoryPosition(): number | undefined {
-    this._trendCategoryPosition = this._devJSON.trendCategoryPosition
-    return this._trendCategoryPosition
-  }
-
-  set trendCategoryPosition(value: number | undefined) {
-    this._trendCategoryPosition = value
+    return this._devJSON.trendCategoryPosition
   }
 
   get status(): any {
-    this._status = this._devJSON.status
-    return this._status
-  }
-
-  set status(value: any) {
-    this._status = value
+    return this._devJSON.status
   }
 
   get stats(): any {
-    this._stats = this._devJSON.stats
-    return this._stats
-  }
-
-  set stats(value: any) {
-    this._stats = value
+    return this._devJSON.stats
   }
 
   get spam(): number | undefined {
-    this._spam = this._devJSON.spam
-    return this._spam
-  }
-
-  set spam(value: number | undefined) {
-    this._spam = value
+    return this._devJSON.spam
   }
 
   get normalize(): number[] {
-    this._normalize = this._devJSON.normalize
-    return this._normalize
-  }
-
-  set normalize(value: number[]) {
-    this._normalize = value
+    return this._devJSON.normalize
   }
 
   get lang(): string | undefined {
-    this._lang = this._devJSON.lang
-    return this._lang
-  }
-
-  set lang(value: string | undefined) {
-    this._lang = value
+    return this._devJSON.lang
   }
 
   get details(): any {
-    this._details = this._devJSON.details
-    return this._details
-  }
-
-  set details(value: any) {
-    this._details = value
+    return this._devJSON.details
   }
 
   get channelId(): string | undefined {
-    this._channelId = this._devJSON.channelId
-    return this._channelId
-  }
-
-  set channelId(value: string | undefined) {
-    this._channelId = value
+    return this._devJSON.channelId
   }
 
   get categoryId(): string | undefined {
-    this._categoryId = this._devJSON.categoryId
-    return this._categoryId
-  }
-
-  set categoryId(value: string | undefined) {
-    this._categoryId = value
+    return this._devJSON.categoryId
   }
 }
